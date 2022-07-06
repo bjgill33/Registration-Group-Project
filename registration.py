78a37ed47d24ff021af5cab62ae0e1f9b28fdeb6
wolfpack83 <108535929+wolfpack83@users.noreply.github.com>
2022-07-06 09:43:29 -0400

    Update registration.py

--- registration.py
+++ registration.py
@@ -1,23 +1,26 @@
-#
-#
-#
-#
-#
+# ----------------------------------------------------------------
+# Author: Brian Gill, Jeremy Jacobs, Joshua Macy
+# Date: 1 July 2022
+# Purpose: Course registration system for students
+# -----------------------------------------------------------------
 
+# import two function defining modules
+import student, billing
+
+
+# validate PIN to student ID
 def login(id, s_list):
     pin = input('Enter PIN: ')
-    validate = False
-    while not validate:
-        for x in range(len(s_list)):
-            if s_list[x][1] == pin:
-                validate = True
-                print('ID and PIN verified\n')
-                return True
-        print('ID or PIN incorrect\n')
-        return False
+    for x in range(len(s_list)):
+        if s_list[x][1] == pin and s_list[x][0] == id:
+            print('ID and PIN verified\n')
+            return True
+    print('ID or PIN incorrect\n')
+    return False
 
 
 def main():
+    # predefined data
     student_list = [('1001', '111'), ('1002', '222'), ('1003', '333'), ('1004', '444')]
 
     student_in_state = {'1001': True,
@@ -32,7 +35,7 @@ def main():
                      'CSC103': ['1002'],
                      'CSC104': []}
     course_max_size = {'CSC101': 3, 'CSC102': 2, 'CSC103': 1, 'CSC104': 3}
-
+    # menu system
     user_id = '1'
     while user_id != '0':
         user_id = input('Enter ID to log in, or 0 to quit: ')
@@ -41,26 +44,38 @@ def main():
         else:
             valid = login(user_id, student_list)
         if valid:
-            choice = input('Enter 1 to add course, 2 to drop course, 3 to list courses, 4 to show bill, 0 to exit:')
+            choice = input('Enter 1 to add course, 2 to drop course, 3 to list courses, 4 to show bill, 0 to exit: ')
             while choice != '0':
                 if choice == '1':
-                    print('1')  # add choice fx
-                    choice = input('Enter 1 to add course, 2 to drop course, 3 to list courses, 4 to show bill, 0 to exit:')
+                    student.add_course(user_id, course_roster, course_max_size)
+                    print()
+                    choice = input(
+                        'Enter 1 to add course, 2 to drop course, 3 to list courses, 4 to show bill, 0 to exit: ')
                 elif choice == '2':
-                    print('2')  # add fx
-                    choice = input('Enter 1 to add course, 2 to drop course, 3 to list courses, 4 to show bill, 0 to exit:')
+                    student.drop_course(user_id, course_roster)
+                    print()
+                    choice = input(
+                        'Enter 1 to add course, 2 to drop course, 3 to list courses, 4 to show bill, 0 to exit: ')
                 elif choice == '3':
-                    print('3')  # add fx
-                    choice = input('Enter 1 to add course, 2 to drop course, 3 to list courses, 4 to show bill, 0 to exit:')
+                    student.list_courses(user_id, course_roster)
+                    print()
+                    choice = input(
+                        'Enter 1 to add course, 2 to drop course, 3 to list courses, 4 to show bill, 0 to exit: ')
                 elif choice == '4':
-                    print('4')  # add fx
-                    choice = input('Enter 1 to add course, 2 to drop course, 3 to list courses, 4 to show bill, 0 to exit:')
+                    hours, cost = billing.calculate_hours_and_bill(user_id, student_in_state, course_roster,
+                                                                   course_hours)
+                    billing.display_hours_and_bill(hours, cost)
+                    print()
+                    choice = input(
+                        'Enter 1 to add course, 2 to drop course, 3 to list courses, 4 to show bill, 0 to exit: ')
                 else:
                     print("Invalid choice")
-                    choice = input('Enter 1 to add course, 2 to drop course, 3 to list courses, 4 to show bill, 0 to exit:')
+                    print()
+                    choice = input(
+                        'Enter 1 to add course, 2 to drop course, 3 to list courses, 4 to show bill, 0 to exit: ')
             print('Session ended')
             print()
 
 
-
 main()
+
